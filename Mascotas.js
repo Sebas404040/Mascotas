7c49f09e0b405894a2f96637370585d3b1d3b192
Sebas404040 <delgadopalitogarbage@gmail.com>
2025-05-26 08:54:11 -0500

    feat: :sparkles: Register pet
    Register pet function created
    
    none

--- Mascotas.js
+++ Mascotas.js
@@ -1,6 +1,7 @@
 function RegistrarMascota() {
     let nombre = prompt("Ingrese el nombre de la mascota:");
     let decision_Especie = prompt("1. Perro\n2. Gato\n3. Conejo\n4. Hamster\n5. Pez");
+    let Especie = "";
     switch (decision_Especie) {
         case "1":
             Especie = "Perro";
@@ -24,7 +25,27 @@ function RegistrarMascota() {
             break;
         default:
             alert("Opción no válida. Por favor, elige una especie válida.");
+            return; 
     }
-}
 
+    let edad = prompt("Ingrese la edad de la mascota (en años):");
+    let peso = prompt("Ingrese el peso de la mascota (en kg):");
+    peso = parseFloat(peso);
+    if (peso < 0 || peso >= 500 || isNaN(peso)) {
+        alert("ERROR: El peso debe ser un número positivo y menor a 500 kg.");
+        return;
+    }
+
+    mascota = {
+        nombre: nombre,
+        Especie: Especie,
+        edad: parseInt(edad),
+        peso: peso
+    }
+
+    alert(
+        `Mascota registrada:\nNombre: ${nombre}\nEspecie: ${Especie}\nEdad: ${edad} años\nPeso: ${peso} kg`
+    );
+}
+Mascotas = [];
 RegistrarMascota();
